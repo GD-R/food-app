35955a9dfdfeea66377bfbe1ef280c3835e034ed
GD-R <82207970+GD-R@users.noreply.github.com>
2022-04-30 00:32:30 +0530

    Update Sign.tsx
     Line 9:14:  'account' is assigned a value but never used  @typescript-eslint/no-unused-vars

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -6,7 +6,7 @@ const Sign = () => {
   async function onSubmitClick(email:string, password: string, displayName?: string) {
      try {
       const auth = getAuth();
-       const account = await createUserWithEmailAndPassword(auth,email,password);
+       await createUserWithEmailAndPassword(auth,email,password);
        if(auth.currentUser) {
         await updateProfile(auth.currentUser, {displayName: displayName})
        }
