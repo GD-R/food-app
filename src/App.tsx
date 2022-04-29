f8df452a584754a36cacfd1c7add6bfa4b51dfb7
GD-R <82207970+GD-R@users.noreply.github.com>
2022-04-30 00:01:39 +0530

    Add files via upload
    src files

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { ConfigureStore } from './components/AppState';
+import Home from './components/Home';
+import Login from './components/Login';
+import Sign from './components/Sign';
+import UI from './components/UI';
+import './components/FireBaseSetup'
+import UserContext from './components/UserContext';
+import Profile from './components/Profile';
+
+
+function App() {
+  return (
+    <div >
+      <Provider store={ConfigureStore()}>
+        <UserContext>
+      <Routes>
+        <Route path='*' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/signup' element={<Sign/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      </UserContext>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
